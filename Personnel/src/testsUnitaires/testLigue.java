96b32fdee344e14834d75afcc62ebf96f8325c7b
selimabenkebaier <islambenkebaier@gmail.com>
2020-01-10 11:12:24 +0100

    Add test setNom et getAdministrateur

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -6,20 +6,48 @@ import org.junit.jupiter.api.Test;
 
 import personnel.*;
 
-class testLigue 
+class testLigue
 {
-	@Test
-	void createLigue() 
-	{
-		Ligue ligue = new Ligue("Fléchettes");
-		assertEquals("Fléchettes", ligue.getNom());
-	}
+        @Test
+        void testCreateLigue()
+        {       
+                Ligue ligue = new Ligue("Fléchettes");
+                assertEquals("Fléchettes", ligue.getNom());
+        }
 
-	@Test
-	void addEmploye() 
-	{
-		Ligue ligue = new Ligue("Fléchettes");
-		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
-		assertEquals(employe, ligue.getEmployes().first());
-	}
+        @Test
+        void testAddEmploye()
+        {
+                Ligue ligue = new Ligue("Fléchettess");
+                Employe employe = ligue.addEmploye("Bouchard", "G�rard", "g.bouchard@gmail.com", "azerty");
+                assertEquals(employe, ligue.getEmployes().first());
+        }
+        @Test
+        void testSetNom()
+        {
+                Ligue ligue = new Ligue("champion");
+                String nom = "selima";
+                Employe employe = ligue.addEmploye(nom, "selima", "sbk@gmail.com", "sbk");
+                assertTrue(employe.getNom().contains(nom));
+        }
+        @Test
+        void  testGetAdministrateur()
+        {
+                Ligue ligue = new Ligue("Fléchettes");
+                Employe employe = GestionPersonnel.getGestionPersonnel().getRoot();
+                ligue.setAdministrateur(employe);
+                assertEquals(employe, ligue.getAdministrateur());
+        }
+        @Test
+        void testRemove()
+        {
+                Ligue ligue = new Ligue("Fléchettes");
+                Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+                employe.remove();
+                assertFalse(ligue.getEmployes().contains(employe));
+        }
+
+  
+
 }
+
