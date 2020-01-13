c6411f024d6c9f0d7d449542eb06c65855039351
yve99 <nguetcheyvecarelle@gmail.com>
2020-01-13 17:11:54 +0100

    Add testSetPrenom & SetPassword

--- Personnel/src/testsUnitaires/testEmploye.java
+++ Personnel/src/testsUnitaires/testEmploye.java
@@ -14,7 +14,33 @@ import personnel.*;
      {
              Ligue ligue = new Ligue("champion");
              String mail = "sbk@gmail.com";
-             Employe employe = ligue.addEmploye(mail, "selima", "sbk@gmail.com", "sbk");
-             assertTrue(employe.getMail().contains(mail));
+             Employe employe = ligue.addEmploye("dénim" ,"selima", mail , "sbk");
+             employe.setMail(mail);
+             assertEquals(mail, employe.getMail());
      }
+	  
+	 @Test
+	 void testSetPassword()
+	 {
+		 
+		 Ligue ligue = new Ligue("champion");
+		 String password = "qwerty";
+		 Employe employe = ligue.addEmploye("dénim", "selima", "sbk@gmail.com", password);
+		 employe.setPassword(password);
+		 assertTrue(employe.checkPassword(password));
+	 }
+	 @Test
+	 void testSetPrenom() {
+		 
+		 Ligue ligue = new Ligue ("champion");
+		 String prenom = "selima";
+		 Employe employe = ligue.addEmploye("dénim", prenom, "sbk@gmail.com", "qwerty");
+		 employe.setPrenom(prenom);
+		 assertEquals(prenom, employe.getPrenom());
+		 
+		 
+		 
+		 
+		 
+	 }
 }
