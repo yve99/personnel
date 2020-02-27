724445d890aaa6820c84d3c6f37d61292d4714ed
selimabenkebaier <islambenkebaier@gmail.com>
2020-02-27 22:22:59 +0100

    DATE

--- Personnel/src/commandLine/EmployeConsole.java
+++ Personnel/src/commandLine/EmployeConsole.java
@@ -1,88 +1,143 @@
 package commandLine;
 
+ 
+
+import static commandLineMenus.rendering.examples.util.InOut.getInt;
+
 import static commandLineMenus.rendering.examples.util.InOut.getString;
 
+ 
+
 import java.time.LocalDate;
-import java.util.ArrayList;
 
-import commandLineMenus.List;
+ 
+
 import commandLineMenus.Menu;
+
 import commandLineMenus.Option;
+
 import personnel.Employe;
+
 import personnel.GestionPersonnel;
-import personnel.Ligue;
 
+ 
 
-public class EmployeConsole 
+ 
+
+public class EmployeConsole
+
 {
-	private Option afficher(final Employe employe)
-	{
-		return new Option("Afficher l'employé", "l", () -> {System.out.println(employe);});
-	}
 
-	Menu editerEmploye(Employe employe)
-	{
-		Menu menu = new Menu("Gérer le compte " + employe.getNom(), "c");
-		menu.add(afficher(employe));
-		menu.add(changerNom(employe));
-		menu.add(changerPrenom(employe));
-		menu.add(changerMail(employe));
-		menu.add(changerPassword(employe));
-//		menu.add(changerDateArrival(employe));
-//		menu.add(changerDateDepart(employe));
-		menu.addBack("q");
-		return menu;
-	}
+             private Option afficher(final Employe employe)
 
-//	private Option changerDateDepart( final Employe employe) {
-//		
-//		return new Option ("changer la date d'arrivée " , "z", () -> {employe.setArrival(getDate());});
-//	}
-//
-//	private LocalDate getDate() {
-//		
-//		int dayOfMonth = 0 ;
-//		int month = 0;
-//		int year = 0;
-//		LocalDate date = LocalDate.of(year, month, dayOfMonth);
-//		return date;               
-//	}
-//
-//	private Option changerDateArrival(final Employe employe) {
-//		
-//		return new Option ("changer la date de départ ", "y",() ->  {employe.setDepart(getDate());});
-//	}
+             {
 
-	private Option changerNom(final Employe employe)
-	{
-		return new Option("Changer le nom", "n", 
-				() -> {employe.setNom(getString("Nouveau nom : "));}
-			);
-	}
-	
-	private Option changerPrenom(final Employe employe)
-	{
-		return new Option("Changer le prénom", "p", () -> {employe.setPrenom(getString("Nouveau prénom : "));});
-	}
-	
-	private Option changerMail(final Employe employe)
-	{
-		return new Option("Changer le mail", "e", () -> {employe.setMail(getString("Nouveau mail : "));});
-	}
-	
-	private Option changerPassword(final Employe employe)
-	{
-		return new Option("Changer le password", "x", () -> {employe.setPassword(getString("Nouveau password : "));});
-	}
-	
-	 Menu selectionnerEmploye(Employe employe) {
-		Menu menu = new Menu("selectionner " + employe.getNom(), "s");
-		menu.add(afficher(employe));
-		menu.add(editerEmploye(employe));
-//		menu.add(supprimerEmploye(employe));
-		menu.addBack("q");
-		
-		return menu;
-	}
+                           return new Option("Afficher l'employé", "l", () -> {System.out.println(employe);});
+
+             }
+
+ 
+
+             Menu editerEmploye(Employe employe)
+
+             {
+
+                           Menu menu = new Menu("Gérer le compte " + employe.getNom(), "c");
+
+                           menu.add(afficher(employe));
+
+                           menu.add(changerNom(employe));
+
+                           menu.add(changerPrenom(employe));
+
+                           menu.add(changerMail(employe));
+
+                           menu.add(changerPassword(employe));
+
+                           menu.add(changerDateDebut(employe));
+
+                           menu.add(changerDateFin(employe));
+
+                           menu.addBack("q");
+
+                           return menu;
+
+             }
+
+ 
+
+             private Option changerNom(final Employe employe)
+
+             {
+
+                           return new Option("Changer le nom", "n",
+
+                                                     () -> {employe.setNom(getString("Nouveau nom : "));}
+
+                                        );
+
+             }
+
+            
+
+             private Option changerPrenom(final Employe employe)
+
+             {
+
+                           return new Option("Changer le prénom", "p", () -> {employe.setPrenom(getString("Nouveau prénom : "));});
+
+             }
+
+            
+
+             private Option changerMail(final Employe employe)
+
+             {
+
+                           return new Option("Changer le mail", "e", () -> {employe.setMail(getString("Nouveau mail : "));});
+
+             }
+
+            
+
+             private Option changerPassword(final Employe employe)
+
+             {
+
+                           return new Option("Changer le password", "x", () -> {employe.setPassword(getString("Nouveau password : "));});
+
+             }
+
+            
+
+             private Option changerDateDebut(final Employe employe) {
+
+                           return new Option("changer la date", "a",() ->  {employe.setDateDebut(getDate());});
+
+             }
+
+            
+
+             private Option changerDateFin(final Employe employe) {
+
+                           return new Option("changer la date", "f",() ->  {employe.setDateFin(getDate());});
+
+             }
+
+             private LocalDate getDate() {/// rapeler la fonction getDate
+
+                           int year = getInt("year : ");
+
+                           int month = getInt("mois : ");
+
+                           int day = getInt(" day :");
+
+                           LocalDate InputDate = LocalDate.of(year, month, day);
+
+                           return InputDate;
+
+                          
+
+             }
 
 }
